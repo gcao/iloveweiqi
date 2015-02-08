2b66b1757cc13e52e382cd8706f5f50429be696f
Guoliang Cao <gcao99@gmail.com>
2015-02-07 20:27:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# I Love Weiqi (for Google Chrome browser)
+
+
+This Chrome extention can enhance game replay functionality on several popular Weiqi forums and websites.
+
+## Installation
+
+It can be installed from [Chrome Web Store](https://chrome.google.com/webstore/detail/???).
+
+## Usage
+
+## Misc
+
+TODO: refactor this to support creating Google Chrome addon and Firefox plugin in this project
+
---
+++ iloveweiqi.js
@@ -0,0 +1,35 @@
+// Try to make code here work on all browsers
+// Put browser specific code in another js (e.g. chrome.js, firefox.js)
+
+//console.log('iloveweiqi')
+
+var URL = "http://localhost:8000/jsgameviewer/js/test.js";
+var SCRIPT_ID = "iloveweiqi-script";
+
+// Add a cache id param to url to force reload this script every hour
+if (URL.indexOf('?') < 0) {
+  URL += "?";
+} else if (URL.indexOf('&') > 0) {
+  URL += "&";
+}
+URL += "cacheId=" + timeToString();
+
+//console.log(URL);
+
+var script = document.createElement("script")
+script.setAttribute("id", SCRIPT_ID);
+script.type = "text/javascript";
+script.src = URL;
+document.documentElement.lastChild.appendChild(script);
+
+function timeToString() {
+  var time = new Date();
+  return padStr(time.getFullYear()) +
+         padStr(1 + time.getMonth()) +
+         padStr(time.getDate()) +
+         padStr(time.getHours());
+}
+
+function padStr(i) {
+  return (i < 10) ? "0" + i : "" + i;
+}
